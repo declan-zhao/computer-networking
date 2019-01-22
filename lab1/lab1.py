f63f74e3ceef4ef78631b2ad70db3a0a135a9dae
Declan Zhao <zydc2010@gmail.com>
2019-01-21 21:50:26 -0500

    feat(lab1): add generate_random

---
+++ lab1/lab1.py
@@ -0,0 +1,15 @@
+import math
+import random
+
+
+def generate_random(lambda_):
+    return -math.log(1 - random.uniform(0, 1))/lambda_
+
+
+def main():
+    # TODO: main
+    return
+
+
+if __name__ == "__main__":
+    main()
